95710a5b8a6eaf7e6f010d5b7ecf318dc22a3f36
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2026-03-23 17:44:32 +0100

    ssh folder for zsh and obsidian folder for nvim

--- extra.sh
+++ extra.sh
@@ -14,3 +14,19 @@ fi
 hyprpm update
 hyprpm add https://github.com/hyprwm/hyprland-plugins
 hyprpm enable hyprexpo
+
+
+# ==============================================================================
+# Create ~/.ssh folder with 700 permissions if it doesn't exist.
+# ==============================================================================
+if [[ ! -d "$HOME/.ssh" ]]; then
+	mkdir -p "$HOME/.ssh"
+	chmod 700 "$HOME/.ssh"
+fi
+
+# ==============================================================================
+# Create ~/.obsidian/Notes folder if it doesn't exist.
+# ==============================================================================
+if [[ ! -d "$HOME/.obsidian/Notes" ]]; then
+	mkdir -p "$HOME/.obsidian/Notes"
+fi
