b3c55d14cd3a357606025328890e3baceffe1f0d
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2026-03-26 10:49:41 +0100

    updated nvim

--- nvim/.config/nvim/lua/shadow/core/init.lua
+++ nvim/.config/nvim/lua/shadow/core/init.lua
@@ -3,7 +3,3 @@ require("shadow.core.remap")
 require("shadow.core.latex")
 require("shadow.core.override.lazygit")
 require("shadow.core.override.snack")
-require("shadow.core.multimark")
-require("shadow.core.vimium").setup({
-	keymap = "ff"
-})
