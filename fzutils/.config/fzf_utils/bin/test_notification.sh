2fc861044f1321c7af87509b9663487d35c8ba8d
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2026-03-29 01:10:43 +0100

    revisited notification test script

--- fzutils/.config/fzf_utils/bin/test_notification.sh
+++ fzutils/.config/fzf_utils/bin/test_notification.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
-notify-send "Test Notification" "This is a test notification from the script." -i "dialog-information"
+OPTIONS=("Normal" "Critical" "Normal with actions" "Critical with actions")
+SELECTED=$(printf '%s\n' "${OPTIONS[@]}" | fzf --prompt="Select notification type: " --border)
+
+case $SELECTED in
+		"Normal")
+				notify-send 'Adam Jefferson' 'Hey, How are you?' -i /usr/share/icons/Tela-circle-dracula/scalable/apps/telegram.svg
+				;;
+		"Critical")
+				notify-send 'Adam Jefferson' 'Hey, How are you?' -i /usr/share/icons/Tela-circle-dracula/scalable/apps/telegram.svg -u 'critical'
+				;;
+		"Normal with actions")
+				bash -c "notify-send 'Adam Jefferson' 'Hey, How are you?' -i /usr/share/icons/Tela-circle-dracula/scalable/apps/telegram.svg --action 'reply=Reply' --action 'ignore=Ignore'"
+				;;
+		"Critical with actions")
+				bash -c "notify-send 'Adam Jefferson' 'Hey, How are you?' -i /usr/share/icons/Tela-circle-dracula/scalable/apps/telegram.svg --action 'reply=Reply' --action 'ignore=Ignore' -u 'critical'"
+				;;
+esac
+
