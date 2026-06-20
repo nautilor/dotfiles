3f2d32e5c279d78ec84e9bdcc6026c43c887696d
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2026-06-21 01:31:49 +0200

    backlight for laptop keyboard script

---
+++ hypr/.config/hypr/bin/backlight.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+
+if ! command -v asusctl &> /dev/null; then
+	notify-send -i "error" "asusctl" "Required dependencies are not installed!"
+	echo "Please install 'asusctl' to use this script."
+	exit 1
+fi
+
+TEMP_FILE="/tmp/asusctl_leds_temp"
+
+function set() {
+	local state="$1"
+	asusctl leds set "$state"
+	echo "$state" > "$TEMP_FILE"
+}
+
+function init() {
+	set "off"
+}
+
+current_state=$(cat "$TEMP_FILE" 2>/dev/null || echo "off")
+case "$current_state" in
+	off)
+		set "low"
+		;;
+	low)
+		set "med"
+		;;
+	med)
+		set "high"
+		;;
+	high)
+		set "off"
+		;;
+	*)
+		init
+		;;
+esac
+
+
